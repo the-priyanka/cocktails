9f31cacc22bf677c731e1085d721438bf86ce210
Priyanka <original.manaria@gmail.com>
2022-02-28 11:27:01 +0530

    about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
+        sit officia veniam. Accusantium neque reprehenderit adipisci
+        alias nesciunt cum expedita delectus maiores deserunt nobis
+        voluptas, enim earum, labore commodi aut aspernatur aliquam.
+        Recusandae corporis eius sed doloribus excepturi molestias
+        quod?
+      </p>
+    </section>
+  );
 };
 
 export default About;
