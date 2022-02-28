14fc9630557db0df0d0fb808690af9b1bd99bcf6
Priyanka <original.manaria@gmail.com>
2022-02-28 13:42:22 +0530

    display list

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Cocktail = () => {
-  return <div>Cocktail</div>;
+const Cocktail = (props) => {
+  return (
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={props.image} alt={props.name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{props.name}</h3>
+        <h4>{props.glass}</h4>
+        <p>{props.info}</p>
+        <Link
+          to={`/cocktail/${props.id}`}
+          className="btn btn-primary"
+        >
+          details
+        </Link>
+      </div>
+    </article>
+  );
 };
 
 export default Cocktail;
--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "../context";
+import Cocktail from "./Cocktail";
 import Loading from "./Loading";
 
 const CocktailList = () => {
@@ -16,7 +17,16 @@ const CocktailList = () => {
       </h2>
     );
   }
-  return <div>CocktailList</div>;
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default CocktailList;
